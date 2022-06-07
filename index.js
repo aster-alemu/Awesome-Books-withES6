4a281b0aeb2ce1d283c0bbdfcdff7d30fecd6fd3
aster-alemu <leaster2014w@gmail.com>
2022-06-07 06:56:47 -0700

    fix linter errors

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 /* eslint-disable no-unused-vars */
 // Select the Links to the pages
+
+import {
+  title, author, loggedBooks, books, showBook
+} from './modules/showbookfunction.js';
+
 const bookLink = document.querySelector('nav ul li .book-link');
 const formLink = document.querySelector('nav ul li .form-link');
 const contactLink = document.querySelector('nav ul li .contact-link');
@@ -8,7 +13,6 @@ const contact = document.querySelector('.contact');
 const form = document.querySelector('form');
 const currentDate = document.querySelector('.date');
 const pageHeading = document.querySelector('.main-heading');
-import { title, author, loggedBooks, books, showBook } from './modules/showbookfunction.js';
 
 // import { author } from './modules/showbookfunction.js';
 // import { loggedBooks } from './modules/showbookfunction.js';
